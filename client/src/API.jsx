cc70cc17e810504e7ad6a276b38645f4b794bcd5
Domenico <gagliardo9975@gmail.com>
2024-01-22 13:56:34 +0100

    add credential include in all API for ngrok

--- client/src/API.jsx
+++ client/src/API.jsx
@@ -6,26 +6,26 @@ import { SERVER_URL } from "./components/Costants";
  * It executes the log-in.
  */
 async function logIn(credentials) {
-    const response = await fetch(SERVER_URL + '/sessions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',  // this parameter specifies that authentication cookie must be forwared
-      body: JSON.stringify(credentials),
-    }).catch(() => {throw {error: "Connection Error"}});
-    if (response.ok) {
-      // 200 status code, parse and return the object
-      const user = await response.json();
-      return ({
-        username: user.username,
-        isRestaurateur: user.isRestaurateur
-      });
-    } else {
-      // json object provided by the server with the error
-      const error = await response.json();
-      throw error;
-    }
+  const response = await fetch(SERVER_URL + '/sessions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',  // this parameter specifies that authentication cookie must be forwared
+    body: JSON.stringify(credentials),
+  }).catch(() => { throw { error: "Connection Error" } });
+  if (response.ok) {
+    // 200 status code, parse and return the object
+    const user = await response.json();
+    return ({
+      username: user.username,
+      isRestaurateur: user.isRestaurateur
+    });
+  } else {
+    // json object provided by the server with the error
+    const error = await response.json();
+    throw error;
+  }
 };
 
 /**
@@ -33,41 +33,41 @@ async function logIn(credentials) {
  * It returns a JSON object with the user info.
  */
 async function getUserInfo() {
-    const response = await fetch(SERVER_URL + '/sessions/current', {
-      // this parameter specifies that authentication cookie must be forwared
-      credentials: 'include'
-    }).catch(() => {throw {error: "Connection Error"}});
-    if (response.ok) {
-      // 200 status code, parse and return the object
-      const user = await response.json();
-      return ({
-        username: user.username,
-        isRestaurateur: user.isRestaurateur
-      });
-    } else {
-      // json object provided by the server with the error
-      const error = await response.json();
-      throw error;
-    }
+  const response = await fetch(SERVER_URL + '/sessions/current', {
+    method: 'GET',
+    credentials: 'include' // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
+  if (response.ok) {
+    // 200 status code, parse and return the object
+    const user = await response.json();
+    return ({
+      username: user.username,
+      isRestaurateur: user.isRestaurateur
+    });
+  } else {
+    // json object provided by the server with the error
+    const error = await response.json();
+    throw error;
+  }
 };
 
 /**
  * This function destroy the current user's session and execute the log-out.
  */
 async function logOut() {
-    const response = await fetch(SERVER_URL + '/sessions/current', {
-      method: 'DELETE',
-      credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
-    }).catch(() => {throw {error: "Connection Error"}});
-    if (response.ok) {
-      // 200 status code, parse and return the object
-      const emptyUser = await response.json();
-      return emptyUser;
-    } else {
-      // json object provided by the server with the error
-      const error = await response.json();
-      throw error;
-    }
+  const response = await fetch(SERVER_URL + '/sessions/current', {
+    method: 'DELETE',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
+  if (response.ok) {
+    // 200 status code, parse and return the object
+    const emptyUser = await response.json();
+    return emptyUser;
+  } else {
+    // json object provided by the server with the error
+    const error = await response.json();
+    throw error;
+  }
 };
 
 /**
@@ -75,7 +75,10 @@ async function logOut() {
  * It returns a JSON object
  */
 async function getUser(username) {
-  const response = await fetch(SERVER_URL + `/users/${username}`).catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + `/users/${username}`, {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const users = await response.json();
@@ -116,7 +119,10 @@ async function updateUser(user) {
  * It returns a JSON object
  */
 async function getRestaurants() {
-  const response = await fetch(SERVER_URL + '/restaurants').catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + '/restaurants', {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const restaurants = await response.json();
@@ -133,7 +139,10 @@ async function getRestaurants() {
  * It returns a JSON object
  */
 async function getRestaurant(restaurantId) {
-  const response = await fetch(SERVER_URL + `/restaurants/${restaurantId}`).catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + `/restaurants/${restaurantId}`, {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const restaurant = await response.json();
@@ -150,7 +159,10 @@ async function getRestaurant(restaurantId) {
  * It returns a JSON object
  */
 async function getInsertedRestaurant() {
-  const response = await fetch(SERVER_URL + '/insertedrestaurants').catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + '/insertedrestaurants', {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const restaurant = await response.json();
@@ -167,7 +179,10 @@ async function getInsertedRestaurant() {
  * It returns a JSON object
  */
 async function getFilteringInfos() {
-  const response = await fetch(SERVER_URL + '/filterinfos').catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + '/filterinfos', {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const filterinfos = await response.json();
@@ -184,7 +199,10 @@ async function getFilteringInfos() {
  * It returns a JSON object
  */
 async function getIngredient(ingredientId) {
-  const response = await fetch(SERVER_URL + `/ingredients/${ingredientId}`).catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + `/ingredients/${ingredientId}`, {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const ingredient = await response.json();
@@ -201,7 +219,10 @@ async function getIngredient(ingredientId) {
  * It returns a JSON object
  */
 async function getDish(id) {
-  const response = await fetch(SERVER_URL + `/dishes/${id}`).catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + `/dishes/${id}`, {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const ingredients = await response.json();
@@ -286,7 +307,10 @@ async function deleteRestaurant(restaurantId) {
  * It returns a JSON object
  */
 async function getReview(review_id) {
-  const response = await fetch(SERVER_URL + `/reviews/${review_id}`).catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + `/reviews/${review_id}`, {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const review = await response.json();
@@ -371,7 +395,10 @@ async function deleteReview(reviewId) {
  * It returns a JSON object
  */
 async function getReviewsByUser(username) {
-  const response = await fetch(SERVER_URL + `/reviews/users/${username}`).catch(() => {throw {error: "Connection Error"}});
+  const response = await fetch(SERVER_URL + `/reviews/users/${username}`, {
+    method: 'GET',
+    credentials: 'include'  // this parameter specifies that authentication cookie must be forwared
+  }).catch(() => { throw { error: "Connection Error" } });
   if (response.ok) {
     // 200 status code, return the object
     const reviews = await response.json();
